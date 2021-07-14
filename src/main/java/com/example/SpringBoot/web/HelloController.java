decee474ebdafc815fa5754328b2b57ba96d94af
15259016978 <1369565664@qq.com>
2021-07-14 14:14:42 +0800

    1.0

---
+++ src/main/java/com/example/SpringBoot/web/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.SpringBoot.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @version 1.0
+ * @author: zhen
+ * @date: 2021/7/12 14:47
+ * @desc:
+ */
+@Controller
+@RequestMapping("/admin")
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello springBoot";
+    }
+}
